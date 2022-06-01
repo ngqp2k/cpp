bf37c8b75ac51ed9fb5338a9d58df57848e3ca57
ngqp2k <ngqp2k@outlook.com>
2022-06-01 13:15:28 +0700

    01.06.22

---
+++ Olympic2022/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n; cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i)
+        cin >> a[i];
+
+    sort(a.begin(), a.end());
+
+    for (int i: a)
+        cout << i << " ";
+
+    return 0;
+}
